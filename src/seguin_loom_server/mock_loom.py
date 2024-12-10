53908bb7bba3bf66129a2d05e1f3d41d3822fc0d
Russell Owen <rowen@uw.edu>
2024-12-10 09:06:53 -0800

    MockLoom: use new MockStream
    and make an async context manager.

--- src/seguin_loom_server/mock_loom.py
+++ src/seguin_loom_server/mock_loom.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
 __all__ = ["MockLoom"]
 
 import asyncio
+from types import TracebackType
+from typing import Type
 
 from .mock_streams import open_mock_connection
 
@@ -14,126 +18,154 @@ class MockLoom:
 
     The user controls this loom by:
 
-    * writing commands to self.cmd_writer, a mock asyncio.StreamWriter
-    * reading from self.reply_reader, a mock asyncio.StreamWriter
+    * writing commands to self.command_stream, a mock asyncio.StreamWriter
+    * reading from self.reply_stream, a mock asyncio.StreamWriter
     """
 
     def __init__(self, verbose: bool = True) -> None:
         self.verbose = verbose
         self.weave_forward = True
-        self.reply_reader, self.cmd_writer = open_mock_connection(
-            async_callback=self.handle_command
-        )
+        self.reply_stream, self.command_stream = open_mock_connection()
         self.done_task: asyncio.Future = asyncio.Future()
         self.error_flag = False
         self.shaft_word = 0
         self.weave_cycle_completed = False
-        # Indicate that the loom is ready
-        self.report_state()
-        self.report_direction()
-        self.report_shafts()
+        asyncio.create_task(self.start())
+
+    async def start(self) -> None:
+        self.read_commands_task = asyncio.create_task(self.handle_commands_loop())
+        await self.report_state()
+        await self.report_direction()
+        await self.report_shafts()
+
+    async def close(self) -> None:
+        self.read_commands_task.cancel()
+        self.reply_stream.close()
+        await self.reply_stream.wait_closed()
 
     @classmethod
     async def amain(cls) -> None:
         loom = cls()
         await loom.done_task
 
-    async def handle_command(self, cmd: str) -> None:
-        if self.verbose:
-            print(f"MockLoom: process client command {cmd!r}")
-        if not cmd:
-            return
-        if cmd[0:1] != "=":
-            print(f"MockLoom: invalid command {cmd!r}: must begin with '='")
-            return
-        if len(cmd) < 2:
-            print(f"MockLoom: invalid command {cmd!r}: must be at least 2 characters")
-            return
-        cmd_char = cmd[1]
-        cmd_data = cmd[2:]
-        match cmd_char:
-            case "C":
-                # Specify which shafts to raise as a hex value
-                try:
-                    self.shaft_word = int(cmd_data, base=16)
-                except Exception:
-                    print(
-                        f"MockLoom: invalid command {cmd!r}: data after =C not a hex value"
-                    )
-                    return
-                if self.verbose:
-                    print(f"MockLoom: raise shafts {self.shaft_word:08x}")
-                self.weave_cycle_completed = False
-                self.report_shafts()
-            case "U":
-                # Client commands unweave on/off
-                # (as opposed to use pushing the button on the loom,
-                #  in which case the loom changes it and reports it to client)
-                if cmd_data == "0":
-                    self.weave_forward = True
+    async def handle_commands_loop(self) -> None:
+        while True:
+            cmdbytes = await self.command_stream.readline()
+            print("MockLoom read command", cmdbytes)
+            if not cmdbytes:
+                break
+            cmd = cmdbytes.decode().rstrip()
+            if self.verbose:
+                print(f"MockLoom: process client command {cmd!r}")
+            if not cmd:
+                return
+            if cmd[0:1] != "=":
+                print(f"MockLoom: invalid command {cmd!r}: must begin with '='")
+                return
+            if len(cmd) < 2:
+                print(
+                    f"MockLoom: invalid command {cmd!r}: must be at least 2 characters"
+                )
+                return
+            cmd_char = cmd[1]
+            cmd_data = cmd[2:]
+            match cmd_char:
+                case "C":
+                    # Specify which shafts to raise as a hex value
+                    try:
+                        self.shaft_word = int(cmd_data, base=16)
+                    except Exception:
+                        print(
+                            f"MockLoom: invalid command {cmd!r}: data after =C not a hex value"
+                        )
+                        return
                     if self.verbose:
-                        print("MockLoom: weave forward commanded by software")
-                elif cmd_data == "1":
-                    self.weave_forward = False
-                    if self.verbose:
-                        print("MockLoom: weave backwards commanded by software")
-                else:
-                    print(f"MockLoom: invalid command {cmd!r}: arg nmust be 0 or 1")
-                    return
-                self.report_direction()
-            case "V":
-                self.reply("=v001")
-            case "Q":
-                self.report_state()
-            case "#":
-                # Out of band command specific to the mock loom.
-                # Cast to lowercase becase uppercase is default on iOS.
-                match cmd_data.lower():
-                    case "d":
-                        self.weave_forward = not self.weave_forward
-                        self.report_direction()
+                        print(f"MockLoom: raise shafts {self.shaft_word:08x}")
+                    self.weave_cycle_completed = False
+                    await self.report_shafts()
+                case "U":
+                    # Client commands unweave on/off
+                    # (as opposed to the user pushing the button on the loom,
+                    # in which case the loom changes it and reports it
+                    # to the client).
+                    if cmd_data == "0":
+                        self.weave_forward = True
                         if self.verbose:
-                            print(
-                                "MockLoom: oob toggle weave direction: "
-                                f"{DIRECTION_NAMES[self.weave_forward]}"
-                            )
-                    case "e":
-                        self.error_flag = not self.error_flag
+                            print("MockLoom: weave forward commanded by software")
+                    elif cmd_data == "1":
+                        self.weave_forward = False
                         if self.verbose:
-                            print(
-                                f"MockLoom: oob toggle loom error flag to {self.error_flag}"
-                            )
-                        self.report_state()
-                    case "n":
-                        if self.verbose:
-                            print("MockLoom: oob request next pick")
-                        self.weave_cycle_completed = True
-                        self.report_state()
-                    case "q":
-                        if self.verbose:
-                            print("MockLoom: oob quit command")
-                        if self.cmd_writer is not None:
-                            self.cmd_writer.close()
-                        self.done_task.set_result(None)
-                    case _:
-                        print(f"MockLoom: unrecognized oob command: {cmd_data!r}")
+                            print("MockLoom: weave backwards commanded by software")
+                    else:
+                        print(f"MockLoom: invalid command {cmd!r}: arg nmust be 0 or 1")
+                        return
+                    await self.report_direction()
+                case "V":
+                    await self.reply("=v001")
+                case "Q":
+                    await self.report_state()
+                case "#":
+                    # Out of band command specific to the mock loom.
+                    # Cast to lowercase becase uppercase is default on iOS.
+                    match cmd_data.lower():
+                        case "d":
+                            self.weave_forward = not self.weave_forward
+                            await self.report_direction()
+                            if self.verbose:
+                                print(
+                                    "MockLoom: oob toggle weave direction: "
+                                    f"{DIRECTION_NAMES[self.weave_forward]}"
+                                )
+                        case "e":
+                            self.error_flag = not self.error_flag
+                            if self.verbose:
+                                print(
+                                    f"MockLoom: oob toggle loom error flag to {self.error_flag}"
+                                )
+                            await self.report_state()
+                        case "n":
+                            if self.verbose:
+                                print("MockLoom: oob request next pick")
+                            self.weave_cycle_completed = True
+                            await self.report_state()
+                        case "q":
+                            if self.verbose:
+                                print("MockLoom: oob quit command")
+                            if self.command_stream is not None:
+                                self.command_stream.close()
+                            self.done_task.set_result(None)
+                        case _:
+                            print(f"MockLoom: unrecognized oob command: {cmd_data!r}")
 
-    def reply(self, reply: str) -> None:
+    async def reply(self, reply: str) -> None:
         """Issue the specified reply, which should not be terminated"""
         if self.verbose:
             print(f"MockLoom: send reply {reply!r}")
-        self.reply_reader.appendline(reply + TERMINATOR)
+        self.reply_stream.write((reply + TERMINATOR).encode())
+        await self.reply_stream.drain()
 
-    def report_direction(self) -> None:
-        self.reply(f"=u{int(not self.weave_forward)}")
+    async def report_direction(self) -> None:
+        await self.reply(f"=u{int(not self.weave_forward)}")
 
-    def report_shafts(self) -> None:
-        self.reply(f"=c{self.shaft_word:08x}")
+    async def report_shafts(self) -> None:
+        await self.reply(f"=c{self.shaft_word:08x}")
 
-    def report_state(self) -> None:
+    async def report_state(self) -> None:
         bitmask = 1
         if self.weave_cycle_completed:
             bitmask += 4
         if self.error_flag:
             bitmask += 8
-        self.reply(f"=s{bitmask:01x}")
+        await self.reply(f"=s{bitmask:01x}")
+
+    async def __aenter__(self) -> MockLoom:
+        await self.start()
+        return self
+
+    async def __aexit__(
+        self,
+        type: Type[BaseException] | None,
+        value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        await self.close()
--- tests/test_mock_loom.py
+++ tests/test_mock_loom.py
@@ -1,104 +1,108 @@
 import asyncio
+import contextlib
 
 from seguin_loom_server.mock_loom import MockLoom
 
 
-async def create_mock_loom() -> MockLoom:
+@contextlib.asynccontextmanager
+async def create_loom():
     """Create a MockLoom and read (and check) the initial replies."""
-    loom = MockLoom()
-    for expected_reply in (
-        b"=s1\r",
-        b"=u0\r",
-        b"=c00000000\r",
-    ):
-        async with asyncio.timeout(1):
-            reply = await read_reply(loom)
-            assert expected_reply == reply
-    assert not loom.cmd_writer.is_closing()
-    assert not loom.reply_reader.at_eof()
-    return loom
+    async with MockLoom() as loom:
+        loom = MockLoom()
+        for expected_reply in (
+            b"=s1\r",
+            b"=u0\r",
+            b"=c00000000\r",
+        ):
+            async with asyncio.timeout(1):
+                reply = await read_reply(loom)
+                assert expected_reply == reply
+        assert not loom.command_stream.is_closing()
+        assert not loom.reply_stream.at_eof()
+        yield loom
 
 
 async def read_reply(loom: MockLoom, timeout: float = 1) -> bytes:
     async with asyncio.timeout(timeout):
-        return await loom.reply_reader.readline()
+        return await loom.reply_stream.readline()
 
 
 async def write_command(loom: MockLoom, command: bytes, timeout: float = 1) -> None:
-    loom.cmd_writer.write(command)
+    loom.command_stream.write(command)
     async with asyncio.timeout(timeout):
-        await loom.cmd_writer.drain()
+        await loom.command_stream.drain()
 
 
 async def test_get_status() -> None:
-    loom = await create_mock_loom()
-    await write_command(loom, b"=Q\r")
-    reply = await read_reply(loom)
-    assert reply == b"=s1\r"
-    assert not loom.done_task.done()
+    async with create_loom() as loom:
+        await write_command(loom, b"=Q\r")
+        reply = await read_reply(loom)
+        assert reply == b"=s1\r"
+        assert not loom.done_task.done()
 
 
 async def test_set_direction() -> None:
-    loom = await create_mock_loom()
-    for direction in (0, 1, 0, 1):
-        await write_command(loom, f"=U{direction:d}\r".encode())
-        reply = await read_reply(loom)
-        assert reply == f"=u{direction:d}\r".encode()
-    assert not loom.done_task.done()
+    async with create_loom() as loom:
+        for direction in (0, 1, 0, 1):
+            await write_command(loom, f"=U{direction:d}\r".encode())
+            reply = await read_reply(loom)
+            assert reply == f"=u{direction:d}\r".encode()
+        assert not loom.done_task.done()
 
 
 async def test_raise_shafts() -> None:
-    loom = await create_mock_loom()
-    for shaftword in (0x0, 0x1, 0x5, 0xFE, 0xFF19, 0xFFFFFFFE, 0xFFFFFFFF):
-        await write_command(loom, f"=C{shaftword:08x}\r".encode())
-        reply = await read_reply(loom)
-        assert reply == f"=c{shaftword:08x}\r".encode()
-    assert not loom.done_task.done()
+    async with create_loom() as loom:
+        for shaftword in (0x0, 0x1, 0x5, 0xFE, 0xFF19, 0xFFFFFFFE, 0xFFFFFFFF):
+            await write_command(loom, f"=C{shaftword:08x}\r".encode())
+            reply = await read_reply(loom)
+            assert reply == f"=c{shaftword:08x}\r".encode()
+        assert not loom.done_task.done()
 
 
 async def test_oob_change_direction() -> None:
-    loom = await create_mock_loom()
-    for expected_direction in (1, 0, 1, 0, 1):
-        cmdchar = "d"
-        if expected_direction == 0:
-            cmdchar = cmdchar.upper()
-        await write_command(loom, f"=#{cmdchar}\r".encode())
-        reply = await read_reply(loom)
-        assert reply == f"=u{expected_direction:d}\r".encode()
-    assert not loom.done_task.done()
+    async with create_loom() as loom:
+        for expected_direction in (1, 0, 1, 0, 1):
+            cmdchar = "d"
+            if expected_direction == 0:
+                cmdchar = cmdchar.upper()
+            await write_command(loom, f"=#{cmdchar}\r".encode())
+            reply = await read_reply(loom)
+            assert reply == f"=u{expected_direction:d}\r".encode()
+        assert not loom.done_task.done()
 
 
 async def test_oob_next_pick() -> None:
-    loom = await create_mock_loom()
-    for i in range(4):
-        cmdchar = "n"
-        if i == 0:
-            cmdchar = cmdchar.upper()
-        await write_command(loom, f"=#{cmdchar}\r".encode())
-        reply = await read_reply(loom)
-        assert reply == b"=s5\r"
-    assert not loom.done_task.done()
+    async with create_loom() as loom:
+        for i in range(4):
+            cmdchar = "n"
+            if i == 0:
+                cmdchar = cmdchar.upper()
+            await write_command(loom, f"=#{cmdchar}\r".encode())
+            reply = await read_reply(loom)
+            assert reply == b"=s5\r"
+        assert not loom.done_task.done()
 
 
 async def test_oob_toggle_error() -> None:
-    loom = await create_mock_loom()
-    for i in range(1, 5):
-        expected_error = bool(i % 2)
-        expected_status_word = 0x01 | (0x08 if expected_error else 0)
-        cmdchar = "e"
-        if i == 0:
-            cmdchar = cmdchar.upper()
-        await write_command(loom, f"=#{cmdchar}\r".encode())
-        assert loom.error_flag == expected_error
-        reply = await read_reply(loom)
-        assert reply == f"=s{expected_status_word:x}\r".encode()
-    assert not loom.done_task.done()
+    async with create_loom() as loom:
+        for i in range(1, 5):
+            expected_error = bool(i % 2)
+            expected_status_word = 0x01 | (0x08 if expected_error else 0)
+            cmdchar = "e"
+            if i == 0:
+                cmdchar = cmdchar.upper()
+            await write_command(loom, f"=#{cmdchar}\r".encode())
+            await asyncio.sleep(0)
+            assert loom.error_flag == expected_error
+            reply = await read_reply(loom)
+            assert reply == f"=s{expected_status_word:x}\r".encode()
+        assert not loom.done_task.done()
 
 
 async def test_oob_quit() -> None:
-    loom = await create_mock_loom()
-    await write_command(loom, b"=#q\r")
-    async with asyncio.timeout(1):
-        await loom.done_task
-    assert loom.cmd_writer.is_closing()
-    assert loom.reply_reader.at_eof()
+    async with create_loom() as loom:
+        await write_command(loom, b"=#q\r")
+        async with asyncio.timeout(1):
+            await loom.done_task
+        assert loom.command_stream.is_closing()
+        assert loom.reply_stream.at_eof()
